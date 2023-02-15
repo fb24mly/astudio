8f5b270155075907888f7b6532e86c92ef312ae7
Fakem1ner <fakem1ner@yandex.ru>
2023-02-15 12:02:53 +0400

    added files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/scripts/script.js
+new Swiper('.reviews__slider', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
+;// CONCATENATED MODULE: ./src/js/app.dev.js
+
+
+
+/******/ })()
+;
